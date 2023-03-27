06ab42ed5077290fcebe044fd7b06772fb322a82
lixelamos <amosbett99@gmail.com>
2023-03-27 10:41:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+
+// Remove main node
+const main = document.querySelector('main#main');
+main.remove();
+
+// Create newHeader node
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+// Append newHeader to body
+document.body.appendChild(newHeader);
